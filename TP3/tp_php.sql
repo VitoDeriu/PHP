776e58a12915f5a061edc1509f3c13b4953391c4
VitoDeriu <vito.deriu@ynov.com>
2025-01-20 23:15:32 +0100

    Ajout des migrations TP3
    Ajout du fichier de création des tables de la base de donnée du TP3.

---
+++ TP3/tp_php.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : lun. 20 jan. 2025 à 22:13
+-- Version du serveur : 8.2.0
+-- Version de PHP : 8.2.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tp_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `interests`
+--
+
+DROP TABLE IF EXISTS `interests`;
+CREATE TABLE IF NOT EXISTS `interests` (
+  `id_interest` int NOT NULL AUTO_INCREMENT,
+  `name_interest` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_interest`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `name_user` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users_interest`
+--
+
+DROP TABLE IF EXISTS `users_interest`;
+CREATE TABLE IF NOT EXISTS `users_interest` (
+  `id_user` int NOT NULL,
+  `id_interest` int NOT NULL,
+  KEY `id_user` (`id_user`),
+  KEY `id_interest` (`id_interest`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
